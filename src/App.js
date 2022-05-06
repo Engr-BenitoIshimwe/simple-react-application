28bb2a02e575f2bad6cedbfa8a1b0cba2f576a6d
Benito Ishimwe <benishimwe31@gmail.com>
2022-05-06 19:12:43 +0200

    react-app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <button onClick={callApi}>Call API</button>
+      </header>
+    </div>
+  );
+}
+
+function callApi() {
+  fetch('https://simple-node-server-benito.herokuapp.com/details', {
+    method: 'GET',
+  })
+    .then((data) => data.json())
+    .then((json) => alert(JSON.stringify(json)));
+}
+
+export default App;
